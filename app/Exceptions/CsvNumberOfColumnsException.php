8feffa75ea4f4593ea17e2907815e6aa92297432
cfm-system-team <66007163+cfm-system-team@users.noreply.github.com>
2020-07-03 18:24:01 +0900

    Add: 店舗・イベントなどの情報をcsvファイルから取り込む機能を追加

---
+++ app/Exceptions/CsvNumberOfColumnsException.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Exceptions;
+
+
+class CsvNumberOfColumnsException extends \Exception
+{
+
+}
---
+++ app/Exceptions/CsvValidationException.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Exceptions;
+
+
+use Throwable;
+
+class CsvValidationException extends \Exception
+{
+
+}
